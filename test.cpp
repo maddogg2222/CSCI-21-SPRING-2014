be42ea5b3a9015256e2472a0016c4d84f4406cbc
Steven Menzies <mad_dogg2222@yahoo.com>
2014-02-10 10:53:46 -0500

    Lab project one

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string userName;
+    cout<<"What is your name?:"<<endl;
+    cin>>userName;
+    cout<<"Hello "<<userName<<"!"<<endl;   
+    return 0;
+}//end main
